9b4fa10d448197f84de00b4505bb124e709d6679
xus <dyxushuai@gmail.com>
2015-01-14 23:43:13 +0800

    init

---
+++ readme.md
@@ -0,0 +1,3 @@
+#### Plan
+
+[mus](https://github.com/JohnSmithX/mus) client with **Angularjs** and **Semantic-UI**
---
+++ src/components/navbar/navbar.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('musNg')
+  .controller('NavbarCtrl', function ($scope) {
+    $scope.date = new Date();
+  });
